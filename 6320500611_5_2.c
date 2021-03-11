f70e09fddcc7307e87a69503ef543aa42bae0d2b
ChargeBrX <panuwat.janj@ku.th>
2021-03-11 15:33:50 +0700

    ดาว

--- 6320500611_5_2.c
+++ 6320500611_5_2.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,j,k,x;
+    int n,i,j,k,x,y;
 
     scanf("%d",&n);
 
     x = n - (n+1)%2;
+    y = (x+1)/2;
 
-    printf("%d",x);
+    for(i=1;i<=n/2;i++)
+    {
+        for(j=1;j<=x;j++)
+        {
+            if(j%y!=0)
+            {
+                printf("_");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
 }
